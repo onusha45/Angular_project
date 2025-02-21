b24a2bb0b96f26561da2766c74496ac2a7a37cb2
hatsimitsu <onusha45@gmail.com>
2025-02-21 16:22:36 +0545

    components are created

--- simple_project/src/app/app.component.ts
+++ simple_project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FirstComponentComponent,SecondComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
