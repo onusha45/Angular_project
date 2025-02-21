b0d2b312cd000d77a3a18b6c35f8b609d36bb075
hatsimitsu <onusha45@gmail.com>
2025-02-21 16:42:49 +0545

    navbar added using bootstrap

--- simple_project/src/app/app.component.ts
+++ simple_project/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FirstComponentComponent,SecondComponentComponent],
+  imports: [RouterOutlet,FirstComponentComponent,SecondComponentComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
