caa7f4257cef6771360de983a68c0e7c9efefff9
hatsimitsu <onusha45@gmail.com>
2025-02-23 16:06:51 +0545

    dropdown using ngfor is added

--- simple_project/src/app/navbar/navbar.component.ts
+++ simple_project/src/app/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+
+dropdown : string[]= ['Dropdown 1st element','Dropdown 2nd element','Dropdown 3rd element','Dropdown 4th element','Dropdown 5th element']
 }
