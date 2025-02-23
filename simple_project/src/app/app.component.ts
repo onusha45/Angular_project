1a3b896d171f79f21374a7b499ad1c30b32c4005
hatsimitsu <onusha45@gmail.com>
2025-02-23 11:59:05 +0545

    Two way databinding added

--- simple_project/src/app/app.component.ts
+++ simple_project/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FirstComponentComponent,SecondComponentComponent,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,FirstComponentComponent,SecondComponentComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- simple_project/src/app/first-component/first-component.component.ts
+++ simple_project/src/app/first-component/first-component.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './first-component.component.html',
   styleUrl: './first-component.component.css'
 })
 export class FirstComponentComponent {
+  email : string = ""
+  password: string =""
+  password_text : string ="Password must be of 8 digits "
+constructor(){
+
+}
+submitAlert(message :string){
+  alert(message)
+}
 
 }
