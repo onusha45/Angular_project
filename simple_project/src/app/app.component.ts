0f5a989b97d2f29fdf505cb9149a8d656b0efeaa
hatsimitsu <onusha45@gmail.com>
2025-02-23 16:26:05 +0545

    toggle button added

--- simple_project/src/app/app.component.ts
+++ simple_project/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from "./footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FirstComponentComponent,SecondComponentComponent,NavbarComponent],
+  imports: [RouterOutlet, FirstComponentComponent, SecondComponentComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- simple_project/src/app/footer/footer.component.ts
+++ simple_project/src/app/footer/footer.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  isVisible : boolean = true;
+
+ enableDisable(){
+  this.isVisible = !this.isVisible;
+ }
 
 }
