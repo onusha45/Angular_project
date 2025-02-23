9257cd9d522745e56158289907673889cf4c1c9d
hatsimitsu <onusha45@gmail.com>
2025-02-23 17:35:40 +0545

    enable & disable using structural directives

--- simple_project/src/app/first-component/first-component.component.ts
+++ simple_project/src/app/first-component/first-component.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first-component',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './first-component.component.html',
   styleUrl: './first-component.component.css'
 })
@@ -11,6 +12,8 @@ export class FirstComponentComponent {
   email : string = ""
   password: string =""
   password_text : string ="Password must be of 8 digits "
+  checkBox: boolean = false;
+
 constructor(){
 
 }
@@ -18,4 +21,7 @@ submitAlert(message :string){
   alert(message)
 }
 
+displayOnUncheck(){
+  this.checkBox = !this.checkBox;
+}
 }
